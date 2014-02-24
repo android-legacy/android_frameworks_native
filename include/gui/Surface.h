91f5bb451ed5fa885a9108056cd86a8adb7ca0e1
erikcas <erikcas1972@gmail.com>
2014-02-24 07:02:57 +0100

    Revert "Add sessionId metadata with every buffer queued through Surface"
    This reverts commit edcb20627cbaa2b80a65f532950948087c18a4b8.
    
    Conflicts:
    	include/gui/Surface.h

--- include/gui/Surface.h
+++ include/gui/Surface.h
@@ -77,9 +77,6 @@ public:
     static bool isValid(const sp<Surface>& surface) {
         return surface != NULL && surface->getIGraphicBufferProducer() != NULL;
     }
-    virtual int32_t getSessionId(){
-        return reinterpret_cast<int>(mGraphicBufferProducer.get());
-    }
     status_t setDirtyRegion(Region* dirty = NULL);
 
 protected:
